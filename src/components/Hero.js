ab69fc9f22e92af4411fc16751bde92ba8139bb9
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-04-26 19:30:28 +0300

    disable tailwindcss & update styles in Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,25 +4,30 @@ import Main1 from "../img/header/home-img-1.jpg";
 import Main2 from "../img/header/home-img-2.jpg";
 import Main3 from "../img/header/home-img-3.jpg";
 import Main4 from "../img/header/home-img-4.jpg";
+import styles from '@/styles/Hero.module.css'
 
 const links = [
-    {id : 1 , title : 'Live Comfortably' , image : Main1 , url : '/'},
-    {id : 2 , title : 'Skincare' , image : Main2 , url : '/'},
-    {id : 3 , title : 'Kitchen' , image : Main3 , url : '/'},
-    {id : 4 , title : 'Electronics' , image : Main4 , url : '/'}
+    {id : 1 , title : 'Live Comfortably' , image : Main1 , url : '/' , grid : 'one'},
+    {id : 2 , title : 'Skincare' , image : Main2 , url : '/', grid : 'two'},
+    {id : 3 , title : 'Kitchen' , image : Main3 , url : '/', grid : 'three'},
+    {id : 4 , title : 'Electronics' , image : Main4 , url : '/', grid : 'four'}
 ]
 const Hero = ()=> {
   return (
-    <div className="pt-48">
-        <div className="grid grid-cols-3 grid-rows-2 gap-5 mt-5 ">
+    <div className={styles.homeContainer}>
+        <div className={styles.gridContainer}>
             {links.map((item)=>{
-                const {id,title,image,url} = item;
+                const {id,title,image,url , grid} = item;
                 return(
-                    <div key={id} className='relative cursor-pointer overflow-hidden'>
+                    <div key={id} className={`${styles.featured} 
+                    ${grid === 'one' ? styles.one
+                    : grid === 'two' ? styles.two
+                    : grid === 'three' ? styles.three
+                    : styles.four}`}>
                         <Link href={url}>
-                            <div className='absolute w-full h-full top-0 left-0 bg-black bg-opacity-30 hover:bg-opacity-50 transition-all duration-300 ease-in z-10'></div>
-                            <Image src={image} alt={title} className='object-cover w-full h-full' />
-                            <p className='absolute bottom-8 left-8 text-white text-5xl font-semibold'>{title}</p>
+                            <div className={styles.overlay}></div>
+                            <Image src={image} alt={title} className={styles.image} />
+                            <p className={styles.description}>{title}</p>
                         </Link>
                     </div>
                 )
