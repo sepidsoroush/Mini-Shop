fb15dc1300dbd932280b621c8a398057a3ef2c8a
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-04-26 20:23:30 +0300

    add Head to app.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
-import { Inter } from 'next/font/google'
 import HomePage from './Home'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-      <HomePage />
-    </main>
+    <>
+      <main>
+        <HomePage />
+      </main>
+    </>
   )
 }
