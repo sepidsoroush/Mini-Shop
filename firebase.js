73ee3c71d864d45770b611131ac79591d3a8b6e9
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-04-26 16:26:29 +0300

    install firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABaktiOeXiHjrDVmxBUPircbXW1gpRWaw",
+  authDomain: "e-commerce-bed4b.firebaseapp.com",
+  projectId: "e-commerce-bed4b",
+  storageBucket: "e-commerce-bed4b.appspot.com",
+  messagingSenderId: "1079748920329",
+  appId: "1:1079748920329:web:27df32bc3500717ae0a4d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
