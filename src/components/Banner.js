d8f667b317f0e0b4161ef77a1583d11125b6fe73
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-04-30 12:07:29 +0300

    update global style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,26 +1,30 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '@/styles/Banner.module.css'
+import { Inter } from 'next/font/google'
 
+const inter = Inter({ subsets: ['latin'] })
 function Banner({ title, text, img ,type}) {
   return (
-    <div className={styles.main}>
-      <div className={styles.container}>
-        <div className={type=== 'normal' ? styles.banner : styles.reverse}>
-          <div className={styles.sidetext}>
-            <div className={styles.text}>
-              <h2>{title}</h2>
-              <p>{text}</p>
-              <Link onClick={() => window.scrollTo(0, 0)} href="">
-                <button>Shop now</button>
-              </Link>
+    <div className={inter.className}>
+        <div className={styles.main}>
+          <div className={styles.container}>
+            <div className={type=== 'normal' ? styles.banner : styles.reverse}>
+              <div className={styles.sidetext}>
+                <div className={styles.text}>
+                  <h2>{title}</h2>
+                  <p>{text}</p>
+                  <Link onClick={() => window.scrollTo(0, 0)} href="">
+                    <button>Shop now</button>
+                  </Link>
+                </div>
+              </div>
+              <div className={styles.image}>
+                <Image src={img} alt="banner" />
+              </div>
             </div>
           </div>
-          <div className={styles.image}>
-            <Image src={img} alt="banner" />
-          </div>
         </div>
-      </div>
     </div>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,6 +5,9 @@ import Main2 from "../img/header/home-img-2.jpg";
 import Main3 from "../img/header/home-img-3.jpg";
 import Main4 from "../img/header/home-img-4.jpg";
 import styles from '@/styles/Hero.module.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 const links = [
     {id : 1 , title : 'Live Comfortably' , image : Main1 , url : '/' , grid : 'one'},
@@ -14,24 +17,28 @@ const links = [
 ]
 const Hero = ()=> {
   return (
-    <div className={styles.homeContainer}>
-        <div className={styles.gridContainer}>
-            {links.map((item)=>{
-                const {id,title,image,url , grid} = item;
-                return(
-                    <div key={id} className={`${styles.featured} 
-                    ${grid === 'one' ? styles.one
-                    : grid === 'two' ? styles.two
-                    : grid === 'three' ? styles.three
-                    : styles.four}`}>
-                        <Link href={url}>
-                            <div className={styles.overlay}></div>
-                            <Image src={image} alt={title} className={styles.image} />
-                            <p className={styles.description}>{title}</p>
-                        </Link>
-                    </div>
-                )
-            })}
+    <div className={inter.className}>
+        <div className={styles.homeContainer}>
+            <div className={styles.container}>
+                <div className={styles.gridContainer}>
+                    {links.map((item)=>{
+                        const {id,title,image,url , grid} = item;
+                        return(
+                            <div key={id} className={`${styles.featured} 
+                            ${grid === 'one' ? styles.one
+                            : grid === 'two' ? styles.two
+                            : grid === 'three' ? styles.three
+                            : styles.four}`}>
+                                <Link href={url}>
+                                    <div className={styles.overlay}></div>
+                                    <Image src={image} alt={title} className={styles.image} />
+                                    <p className={styles.description}>{title}</p>
+                                </Link>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     </div>
   );
