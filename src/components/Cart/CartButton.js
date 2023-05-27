d86c2f14edd6ee64225478854a95feb6039cd18a
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-05-27 22:55:54 +0300

    update cart icon in Navbar

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,38 @@
+import { useContext, useState, useEffect } from "react";
+import styles from "../../styles/CartButton.module.css";
+import { FaShoppingCart } from "react-icons/fa";
+import CartContext from "../../context/cart-context";
+
+const CartButton = (props) => {
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
+  const { items } = useContext(CartContext);
+  const totalOrders = items.reduce((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
+  const btnClasses = `${styles.button} ${btnHighlighted ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={styles.icon}>
+        <FaShoppingCart />
+      </span>
+      <span className={styles.badge}>{totalOrders}</span>
+    </button>
+  );
+};
+
+export default CartButton;
--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -2,27 +2,12 @@ import styles from "@/styles/Navbar.module.css";
 import LogoImg2 from "../../img/newlogo2.png";
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useContext } from "react";
-import { FaShoppingCart } from "react-icons/fa";
 import { Inter } from "next/font/google";
-import CartContext from "@/context/cart-context";
+import CartButton from "../Cart/CartButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Navbar = (props) => {
-  const [sticky, setSticky] = useState(false);
-  const { totalAmount } = useContext(CartContext);
-
-  const handleScroll = () => {
-    if (window.scrollY > 10) {
-      setSticky(true);
-    } else {
-      setSticky(false);
-    }
-  };
-
-  // window.addEventListener("scroll", handleScroll);
-
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -35,41 +20,27 @@ const Navbar = (props) => {
   };
 
   return (
-    <div className={inter.className}>
-      <nav className={styles.navbar}>
-        <div className={styles.container}>
-          <div
-            className={`${styles.navContainer} ${sticky ? styles.sticky : ""}`}
-          >
-            <Link href="/">
-              <Image
-                onClick={scrollToTop}
-                src={LogoImg2}
-                alt="logo"
-                className={styles.logo}
-              />
-            </Link>
-            <div className={styles.links}>
-              <Link
-                onClick={() => window.scrollTo(0, 0)}
-                href="/categories/all"
-              >
-                categories
-              </Link>
-              <Link onClick={() => window.scrollTo(0, 0)} href="/product/19">
-                product page
-              </Link>
-              <div onClick={openCartHandler}>
-                <FaShoppingCart />
-                {totalAmount > 0 && (
-                  <span className={styles.number}>{totalAmount}</span>
-                )}
-              </div>
-            </div>
-          </div>
+    <nav className={`${styles.navbar} ${inter.className}`}>
+      <div className={styles.navContainer}>
+        <Link href="/">
+          <Image
+            onClick={scrollToTop}
+            src={LogoImg2}
+            alt="logo"
+            className={styles.logo}
+          />
+        </Link>
+        <div className={styles.links}>
+          <Link onClick={() => window.scrollTo(0, 0)} href="/categories/all">
+            categories
+          </Link>
+          <Link onClick={() => window.scrollTo(0, 0)} href="/product/19">
+            products
+          </Link>
+          <CartButton onClick={openCartHandler} />
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
